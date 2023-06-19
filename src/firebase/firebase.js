6f56389c6e087740bce945a9c5aa2b00e261f63d
RoxiFOchoaG <46870050+roxifochoag@users.noreply.github.com>
2023-06-19 10:39:42 -0500

    Descripción- Se creo la página de inicio el front-end de registrar usuario e iniciar sesión

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,32 +1,29 @@
-import  { auth } from "./config.js";
-import { createUserWithEmailAndPassword } from "firebase/auth"
-
+import { auth } from "./config.js";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const user = {
-    name: "pepita perez",
-    email: "pepita@perez.com",
-    nick: "pepis",
-    password: "123456"
-}
+  name: "pepita perez",
+  email: "pepita@perez.com",
+  nick: "pepis",
+  password: "123456",
+};
 
+singUp(user);
 
-singUp(user)
+export const singUp = (user) => {
+  createUserWithEmailAndPassword(auth, user.email, user.password)
+    .then((userCredential) => {
+      const userCreated = userCredential.user;
+      console.log(userCredential);
 
-export const singUp =(user) =>{
-   createUserWithEmailAndPassword(auth, user.email, user.password) 
-   .then((userCredential) => {
-    const userCreated = userCredential.user;
-    console.log(userCredential)
-    
-    return userCreated
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
+      return userCreated;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
 
-    console.log(errorCode, " ", errorMessage)
+      console.log(errorCode, "", errorMessage);
 
-    return error
-  });
-
-}
+      return error;
+    });
+};
