388fd8e256aeb51759dab046376a5a95ea1f17af
RoxiFOchoaG <46870050+roxifochoag@users.noreply.github.com>
2023-06-30 12:07:11 -0500

    Creación del archivo Feed.js y mudanza de stylos del feed

---
+++ src/components/Feed.js
@@ -0,0 +1,62 @@
+export const Feed = () =>{
+// ------------------------------------------------
+// Empieza el HeadDiv
+// Creando el head 
+const headDiv= document.createElement('head');
+headDiv.className = 'header colorbackpink';
+// Logo del head
+const logoHead = document.createElement('img');
+logoHead.className = 'logoHead';
+logoHead.src = '/src/img/icon-logo.png';
+logoHead.alt = 'logo';
+// Titulo del head
+const titleHead= document.createElement('title');
+titleHead.innerText = 'WarmiFeed';
+// Cierra el head
+// ------------------------------------------------
+// Inicio del header
+// declaración de la clase del header
+const colorbackpink = document.createDocument('header');
+colorbackpink.className ='colorbackpink';
+// logo de warmi
+const logo = document.createElement('img');
+logo.className = 'logo';
+logo.src = '/src/img/icon-logo.png';
+logo.alt = 'logo';
+// Logo horizontal
+const logohorizontal = document.createElement('img');
+logohorizontal.className = 'logo-horizontal';
+logohorizontal.src = '/src/img/logo-feed.png';
+logohorizontal.alt = 'logo';
+// Estructura del boton
+// Boton 
+const IconHideMenu = document.createElement('button');
+IconHideMenu.className = 'icon-hide-menu';
+
+const IconMenuCelphone = document.createElement('i');
+IconMenuCelphone.src = 'img/icon-menu-celphone.svg';
+//Barra de búsqueda 
+const InputSearchBar = document.createElement('search');
+InputSearchBar.className = 'input-search-bar';
+InputSearchBar.placeholder = 'Buscar';
+// Fin del header
+// ------------------------------------------------
+
+
+
+
+// Centro del Main
+// Div izquierda
+
+// Trending topic
+
+// HashTags
+
+//  Conversaciones
+
+// Noticias
+
+// Div derecha
+
+    
+}
